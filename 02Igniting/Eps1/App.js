d6ece1f6f2ad8770617bbc5e74cd310e56d68fcc
Prashishgt <prashish24@gmail.com>
2023-11-06 15:34:53 +0545

    issue with pc

--- 02Igniting/Eps1/App.js
+++ 02Igniting/Eps1/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 // Header
 // -logo
@@ -12,16 +13,53 @@ import { ReactDOM } from "react";
 //-address
 //-contact
 const Header = () => {
-  return <div className="header">
-    <div>
-        
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-  </div>;
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <h3>Papa's Bakery</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
 };
 
 const AppLayout = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.Render();
+root.render(<AppLayout />);
